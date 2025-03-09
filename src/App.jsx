02a910f5bbae0122e07b21575b8748042975f681
Joye Fu <jfu2664@gmail.com>
2025-03-10 05:20:44 +1100

    deploy i hope

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ const App = () => {
         <div className="bg-white p-6 rounded-lg shadow-lg w-96 text-center flex flex-col justify-center items-center">
           <img className='size-50' src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHpqZzRxZ2Q0d3RxZG5wZmNseWRndDJpNXU1cHIzbXNwbjRwbjB2byZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/x8YjX77exapooAAwNc/giphy.gif" />
           <h2 className="text-xl font-bold">hi! i'm joye</h2>
-          <p className="mt-2">i made this website for compclub subcom apps. i enjoy making silly projects and drawing in my spare time :))</p>
+          <p className="mt-2">this is my website for compclub subcom apps. i enjoy making silly projects and drawing in my spare time :))</p>
         </div>
       </div>
     );
